a77db6894037d76d33d55437802ead09fdb3d89f
Nurik <114292339+NurikD@users.noreply.github.com>
2024-03-08 01:38:55 +0500

    base structure

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,21 @@
 import 'package:flutter/material.dart';
-import 'internal/application.dart';
-
+import 'package:ping_track/project/home.dart';
 
 void main(){
-  runApp(Application());
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        scaffoldBackgroundColor: Color(0xff121112)
+      ),
+      home: MainShell(),
+    );
+  }
 }
